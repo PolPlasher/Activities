67a5bf24ce97fad143ce7a8009ae1ec9bdd198fb
Pol Lleida <pol.lleida@enti.cat>
2024-03-20 00:21:12 +0100

    AA1_03 AGameBoard

---
+++ AA1_03_AGameBoard/header.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <iostream>
+
+#define NUM_ROWS 7
+#define NUM_COLUMNS 7
+const int MAX_COINS =
+(NUM_ROWS * NUM_COLUMNS) *
+0.2; // The maximum amount of coins is the 20% of the total number of cells
+const int MAX_STONES =
+(NUM_ROWS * NUM_COLUMNS) *
+0.3; // The maximum amount of rocks is the 30% of the total number of cells
+
+// Types of movement that the player can do
+enum class Movement { INVALID, UP, DOWN, LEFT, RIGHT };
+
+// Types of elements that can be in the board
+enum class Element { EMPTY, PLAYER, COIN, STONE };
+
+struct Game;
+
+struct Player {
+
+    Game* game_ptr; // Pointer to Game for accessing the parent
+
+    std::pair<int, int>
+        position; // Position of the player <row, col> in the board matrix
+
+    int score; // Score of the player
+
+    // Returns true if the given movement is legal in relation to the player
+    bool checkMovement(Movement mov);
+
+    // Adds one to the player's score
+    void addScore();
+
+    // Sets the player's position to the given row and column
+    void setPos(int row, int col);
+
+    // Returns true if the given movements leads the player to a coin
+    bool existsCoin(Movement mov);
+};
+
+struct Game {
+
+    Player player; // The player
+
+    int num_coins;  // Number of coins in the board
+    int num_stones; // Number of stones in the board
+
+    Element board[NUM_ROWS][NUM_COLUMNS]; // The board of the game
+
+    // Initializes the board with the player, coins and stones
+    void initializeBoard();
+
+    // Moves the player with the mov direction
+    void movePlayer(Movement mov);
+
+    // Returns true if the game is over
+    bool gameOver();
+
+    // Prints the board
+    void printBoard();
+
+    // Asks the input to the player
+    Movement askInput();
+
+    void titleScreen();
+};
